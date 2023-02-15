50b38bdae555da5e39d86f99c2af30f11db8c910
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2023-02-15 19:05:04 +0200

    fixed model of partner

--- models/partner.model.js
+++ models/partner.model.js
@@ -3,29 +3,32 @@ const { SchemaTypes } = mongoose;
 const Schema = mongoose.Schema;
 
 const Partner = new Schema({
-    name: {
-        type: SchemaTypes.String,
-        required: [true, 'Set name for partner'],
-    },
-    email: {
-        type: SchemaTypes.String,
-        required: [true, 'Set email for partner'],
-    },
-    phone: {
-        type: SchemaTypes.String
-        
-    },
-    address: {
-        type: SchemaTypes.String,
-    },
-    avatarURL: {
-        type: SchemaTypes.String,
-        
-    },
-    workTime: {
-        type: SchemaTypes.String,
-        
-    },
+  title: {
+    type: SchemaTypes.String,
+    required: [true, 'Set name for partner'],
+  },
+  url: {
+    type: SchemaTypes.String,
+  },
+  addressUrl: {
+    type: SchemaTypes.String,
+  },
+  imageUrl: {
+    type: SchemaTypes.String,
+  },
+  address: {
+    type: SchemaTypes.String,
+  },
+  workDays: {
+    type: SchemaTypes.Array,
+  },
+  phone: {
+    type: SchemaTypes.String,
+  },
+  email: {
+    type: SchemaTypes.String,
+   
+  },
 });
 
 module.exports = mongoose.model('Partner', Partner);
