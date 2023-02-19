d7a6adee72b97f1452f69baa1a62cecc9dddbdd7
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-19 10:53:31 +0200

    fixed(Ctrl): change res data in News/Friends Ctrl

--- controllers/friends/friendsList.controller.js
+++ controllers/friends/friendsList.controller.js
@@ -2,14 +2,12 @@ const { wrapCtrl } = require('../../middlewares/wrapCtrl');
 const { getPartners } = require('../../services/friends/friendsList.service');
 
 const listPartnersCtrl = async (_, res) => {
-  const partners = await getPartners();
+  const friends = await getPartners();
 
   res.json({
     status: 'success',
     code: 200,
-    data: {
-      partners,
-    },
+    friends,
   });
 };
 
--- controllers/news/newsList.controller.js
+++ controllers/news/newsList.controller.js
@@ -4,9 +4,9 @@ const { getNews } = require('../../services/news/newsList.service');
 const getNewsCtrl = async (req, res) => {
   const { search } = req.query;
 
-  const data = await getNews(search);
+  const news = await getNews(search);
 
-  res.status(200).json({ status: `Successfully!`, statusCode: 200, data });
+  res.status(200).json({ status: `Successfully!`, statusCode: 200, news });
 };
 
 module.exports = {
