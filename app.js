93ba10fce9affc02384182965f989cf1800e2421
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-24 18:52:48 +0200

    fixed(upload size)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 
 const swaggerUI = require('swagger-ui-express');
 const swaggerDoc = require('./swagger.json');
@@ -21,7 +22,7 @@ const { MONGO_URL } = process.env;
 
 app.use(logger(typeLogger));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/api/users', usersRouter);
 app.use('/api/notices', noticesRouter);
@@ -29,6 +30,11 @@ app.use('/api/friends', friendsRouter);
 app.use('/api/news', newsRouter);
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use((_, res) => {
   res.status(404).json({ message: 'Sorry, but this resource not found' });
